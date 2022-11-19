6cd19b75b12f011a794d32feafb8cb6f3b4f1fc5
Alexander Smurov <a13smrv@gmail.com>
2022-11-19 22:30:35 +0100

    feat: make initial project hierarchy
    And also:
    - make logic to dynamically change the tab icon
    - import normalize.css

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+
+function App() {
+  return(
+    <div className="app">
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("root")
+);

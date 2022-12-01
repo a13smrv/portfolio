b3ef218c5fab1b97b66e21e0963440561f2352d2
Alexander Smurov <a13smrv@gmail.com>
2022-12-01 18:04:40 +0100

    feat: make text description in the Profile component

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,28 @@ import "./Profile.scss";
 export function Profile() {
   return (
     <section className="profile">
-      <div className="container"></div>
+      <div className="container">
+        <div className="textDescription">
+          <h2 className="title">👋 Hi, I'm Oleksandr</h2>
+          <p className="introduction text">
+            I'm a <strong>Frontend Developer</strong> with a passion for developing beautiful UIs and learning modern technologies.
+            My mission is to connect businesses and users.
+          </p>
+          <div className="bottomInformationBlock">
+            <p className="placeOfWork text">
+              I'm looking for new opportunities<br />
+              Previously I worked at <a className="link" href="#">Abz.Agency</a>
+            </p>
+            <p className="contacts text">
+              Thun, Switzerland<br />
+              <a className="link" href="mailto:a13smrv@gmail.com">a13smrv@gmail.com</a>
+            </p>
+          </div>
+        </div>
+        <div className="visualDescription">
+          1
+        </div>
+      </div>
     </section>
   )
 }
